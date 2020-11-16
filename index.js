c000ffba8a805c5467c92e1b1668e357f026d05e
Rishav Bharti <rshvbharti@gmail.com>
2020-11-16 13:56:56 +0530

    added router and controller for home

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const router = require('./routes');
 const app = express();
 const port = 8000;
+
+
+// use rountes from express
+app.get('/',require('./routes/index'));
+
+
+
+
 
 app.listen(port, function(err){
     if(err){
